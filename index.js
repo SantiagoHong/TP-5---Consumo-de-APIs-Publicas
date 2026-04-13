7adfe5884e8a28a3ad0ea42811307d5ee27da5a3
SANTIAGO NAHUEL CHOE <49092487@ort>
2026-04-13 11:42:31 -0300

    terminado

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-
-
 async function BuscarPokemon() {
-
-    let Resulstado = document.getElementById("Resultado")
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${ID.value}`)
     const data = await response.json()
     
     document.getElementById("Nombre").innerHTML = data.name
     document.getElementById("Imagen").src = data.sprites.front_default
-    document.getElementById("Tipo").innerHTML = data.types[0].type.name
+    if (data.types.length > 1) {
+    document.getElementById("Tipo").innerHTML = "Type: " + data.types[0].type.name
+    document.getElementById("Tipo2").innerHTML = "Type 2: " + data.types[1].type.name
+    } else {
+    document.getElementById("Tipo").innerHTML = "Type: " + data.types[0].type.name
+    document.getElementById("Tipo2").innerHTML = ""
+    }
+    document.getElementById("Peso").innerHTML = "Weight: " + data.weight + " kg"
+    document.getElementById("Altura").innerHTML = "Height: " + data.height + " ft"
 }             
